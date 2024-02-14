354b90a6b7da4ddbfa0fe45e3e99d94ec1568bda
AHTI6IOTIK <phsts@yandex.ru>
2024-02-14 22:09:05 +0300

    hw02_unpack_string: реализовал ДЗ

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,47 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(st string) (string, error) {
+	var result strings.Builder
+
+	sta := strings.Split(st, "")
+	count := len(sta)
+	next := 1
+
+	for i := 0; i < count; i += next {
+		currentItem := sta[i]
+		_, err := strconv.Atoi(currentItem)
+		if err == nil {
+			return "", ErrInvalidString
+		}
+
+		nextItem := ""
+		size := 0
+
+		if i+1 < count {
+			nextItem = sta[i+1]
+		}
+
+		size, err = strconv.Atoi(nextItem)
+
+		if err != nil {
+			result.WriteString(currentItem)
+			next = 1
+		} else {
+			result.WriteString(strings.Repeat(currentItem, size))
+			next = 2
+		}
+
+		if i+next > count {
+			next = 0
+		}
+	}
+
+	return result.String(), nil
 }
