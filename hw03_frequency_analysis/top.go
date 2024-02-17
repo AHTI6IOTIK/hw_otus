e2a188b99c5916591791ec095044fd4431416d8a
AHTI6IOTIK <phsts@yandex.ru>
2024-02-17 20:12:06 +0300

    hw03_frequency_analysis: выполнил дз

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,107 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type WordRepeat struct {
+	value string
+	count int
+}
+
+type WordRepetitionCounter struct {
+	words []WordRepeat
+	step  int
+}
+
+func New(size int) WordRepetitionCounter {
+	return WordRepetitionCounter{
+		step:  0,
+		words: make([]WordRepeat, size),
+	}
+}
+
+func (a *WordRepetitionCounter) GetWords() []WordRepeat {
+	return a.words
+}
+
+func (a *WordRepetitionCounter) GetStep() int {
+	return a.step
+}
+
+func (a *WordRepetitionCounter) Append(word string) {
+	for i, v := range a.words {
+		if v.value == word {
+			v.count++
+			a.words[i] = v
+
+			return
+		}
+	}
+
+	a.words[a.step] = WordRepeat{
+		value: word,
+		count: 1,
+	}
+
+	a.step++
+}
+
+func (a *WordRepetitionCounter) GetTop(count int) []WordRepeat {
+	if count > a.step {
+		count = a.step
+	}
+
+	takenItems := make(map[int]struct{}, a.step)
+	topItems := make([]WordRepeat, 0, count)
+
+	for len(topItems) < count {
+		selectedValue := 0
+		maxItem := WordRepeat{}
+		for i := 0; i < a.step; i++ {
+			_, alreadySelected := takenItems[i]
+			currentItem := a.words[i]
+			if currentItem.count > maxItem.count && !alreadySelected {
+				maxItem = currentItem
+				selectedValue = i
+			}
+		}
+
+		topItems = append(topItems, a.words[selectedValue])
+		takenItems[selectedValue] = struct{}{}
+	}
+
+	return topItems
+}
+
+func Top10(str string) []string {
+	if str == "" {
+		return []string{}
+	}
+
+	splitStr := strings.Fields(str)
+	wordCounter := New(len(splitStr))
+
+	for _, item := range splitStr {
+		wordCounter.Append(item)
+	}
+
+	top10 := wordCounter.GetTop(10)
+
+	sort.Slice(top10, func(i, j int) bool {
+		if top10[i].count == top10[j].count {
+			return top10[i].value < top10[j].value
+		}
+
+		return top10[i].count > top10[j].count
+	})
+
+	result := make([]string, 0, len(top10))
+
+	for _, v := range top10 {
+		result = append(result, v.value)
+	}
+
+	return result
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -3,6 +3,7 @@ package hw03frequencyanalysis
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -79,4 +80,149 @@ func TestTop10(t *testing.T) {
 			require.Equal(t, expected, Top10(text))
 		}
 	})
+}
+
+func TestByExample(t *testing.T) {
+	str := "cat and dog, one dog,two cats and one man"
+	res := Top10(str)
+
+	expected := []string{
+		"and",     // (2)
+		"one",     // (2)
+		"cat",     // (1)
+		"cats",    // (1)
+		"dog,",    // (1)
+		"dog,two", // (1)
+		"man",     // (1)
+	}
+	require.Equal(t, expected, res)
+}
+
+func TestWordRepetitionCounter_case(t *testing.T) {
+	testCases := []struct {
+		name              string
+		configuredCounter func(size int) WordRepetitionCounter
+		prepareCounter    func(w *WordRepetitionCounter)
+		topRequest        int
+		expectedStep      int
+		expectedTop       []WordRepeat
+		expectedWord      []WordRepeat
+	}{
+		{
+			name: "Тестируем базовую работу функционала",
+			configuredCounter: func(size int) WordRepetitionCounter {
+				wordRepetitionCounter := New(size)
+
+				return wordRepetitionCounter
+			},
+			prepareCounter: func(w *WordRepetitionCounter) {
+				w.Append("1")
+				w.Append("2")
+				w.Append("3")
+				w.Append("4")
+				w.Append("5")
+				w.Append("6")
+			},
+			topRequest:   2,
+			expectedStep: 6,
+			expectedTop: []WordRepeat{
+				{
+					value: "1",
+					count: 1,
+				},
+				{
+					value: "2",
+					count: 1,
+				},
+			},
+			expectedWord: []WordRepeat{
+				{
+					value: "1",
+					count: 1,
+				},
+				{
+					value: "2",
+					count: 1,
+				},
+				{
+					value: "3",
+					count: 1,
+				},
+				{
+					value: "4",
+					count: 1,
+				},
+				{
+					value: "5",
+					count: 1,
+				},
+				{
+					value: "6",
+					count: 1,
+				},
+			},
+		},
+		{
+			name: "Тестируем корректности подсчета слов",
+			configuredCounter: func(size int) WordRepetitionCounter {
+				wordRepetitionCounter := New(size)
+
+				return wordRepetitionCounter
+			},
+			prepareCounter: func(w *WordRepetitionCounter) {
+				w.Append("1")
+				w.Append("2")
+				w.Append("3")
+				w.Append("4")
+				w.Append("5")
+				w.Append("2")
+				w.Append("2")
+			},
+			topRequest:   2,
+			expectedStep: 5,
+			expectedTop: []WordRepeat{
+				{
+					value: "2",
+					count: 3,
+				},
+				{
+					value: "1",
+					count: 1,
+				},
+			},
+			expectedWord: []WordRepeat{
+				{
+					value: "1",
+					count: 1,
+				},
+				{
+					value: "2",
+					count: 3,
+				},
+				{
+					value: "3",
+					count: 1,
+				},
+				{
+					value: "4",
+					count: 1,
+				},
+				{
+					value: "5",
+					count: 1,
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			wordCounter := tc.configuredCounter(tc.expectedStep)
+			tc.prepareCounter(&wordCounter)
+
+			assert.Equal(t, tc.expectedStep, wordCounter.GetStep())
+			assert.Equal(t, tc.expectedTop, wordCounter.GetTop(tc.topRequest))
+			assert.Equal(t, tc.expectedWord, wordCounter.GetWords())
+		})
+	}
 }
