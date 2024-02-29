d64c1590ac6dd540d57794fc19c76d52c8d437d8
AHTI6IOTIK <phsts@yandex.ru>
2024-02-29 22:38:07 +0300

    hw04_lru_cache: реализовал двусвязный список

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,148 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	size int
+	head *ListItem
+	tail *ListItem
+}
+
+func (l *list) Len() int {
+	return l.size
+}
+
+func (l *list) Front() *ListItem {
+	if l.head == nil && l.tail != nil {
+		return l.tail
+	}
+
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	if l.tail == nil && l.head != nil {
+		return l.head
+	}
+
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	listItem := &ListItem{
+		Value: v,
+	}
+
+	if l.size == 1 && l.tail == nil {
+		l.tail = l.head
+		l.head = nil
+	}
+
+	if l.head == nil {
+		l.head = listItem
+	} else {
+		listItem.Next = l.head
+		l.head.Prev = listItem
+		l.head = listItem
+	}
+
+	l.size++
+
+	if l.size == 2 {
+		l.fixRefs()
+	}
+
+	return l.head
+}
+
+// PushBack добавляем элемент в конец
+func (l *list) PushBack(v interface{}) *ListItem {
+	listItem := &ListItem{
+		Value: v,
+	}
+
+	if l.size == 1 && l.head == nil {
+		l.head = l.tail
+		l.tail = nil
+	}
+
+	if l.tail == nil {
+		l.tail = listItem
+	} else {
+		listItem.Prev = l.tail
+		l.tail.Next = listItem
+		l.tail = listItem
+	}
+
+	l.size++
+
+	if l.size == 2 {
+		l.fixRefs()
+	}
+
+	return l.tail
+}
+
+func (l *list) Remove(i *ListItem) {
+	if l.head == l.tail && l.head == i && l.tail == i {
+		l.head = nil
+		l.tail = nil
+		i.Next = nil
+		i.Prev = nil
+		l.size = 0
+
+		return
+	}
+
+	switch i {
+	case l.head:
+		l.head = i.Next
+		if l.head != nil {
+			l.head.Prev = nil
+		}
+	case l.tail:
+		l.tail = i.Prev
+
+		if l.tail != nil {
+			l.tail.Next = nil
+		}
+	default:
+		prevItem := i.Prev
+		nextItem := i.Next
+
+		prevItem.Next = nextItem
+		nextItem.Prev = prevItem
+	}
+
+	l.size--
+}
+
+// MoveToFront переместит элемент в начало
+func (l *list) MoveToFront(i *ListItem) {
+	if l.head == i {
+		return
+	}
+
+	prevItem := i.Prev
+	nextItem := i.Next
+
+	prevItem.Next = nextItem
+
+	if nextItem != nil {
+		nextItem.Prev = prevItem
+	}
+
+	i.Prev = nil
+	i.Next = l.head
+	l.head = i
+}
+
+// fixRefs проставляет ссылки головы и хвоста друг на друга
+func (l *list) fixRefs() {
+	if l.size != 2 {
+		return
+	}
+
+	l.head.Next = l.tail
+	l.tail.Prev = l.head
 }
 
 func NewList() List {
