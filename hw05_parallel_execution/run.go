878ca4c4c3debfd68586288b03527e53f5b4ef04
AHTI6IOTIK <phsts@yandex.ru>
2024-03-10 13:50:10 +0300

    hw05_parallel_execution: упростил решение

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,7 +1,6 @@
 package hw05parallelexecution
 
 import (
-	"context"
 	"errors"
 	"sync"
 )
@@ -12,54 +11,34 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	ctx, cancel := context.WithCancel(context.Background())
-
 	var wg sync.WaitGroup
+	taskChan := make(chan Task)
+	errChan := make(chan struct{}, n+m)
 
-	taskChan := make(chan Task, n)
-	errChan := make(chan struct{}, n)
 	isErrorsExceed := false
-
 	isIgnoreError := m <= 0
 	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go worker(ctx, isIgnoreError, taskChan, errChan, &wg)
+		go worker(isIgnoreError, taskChan, errChan, &wg)
 	}
 
-	wg.Add(1)
-	go func() {
-		step := len(tasks) - 1
-		errCount := 0
-		for !(errCount > m) {
-			select {
-			case <-errChan:
-				errCount++
-				if errCount >= m {
-					isErrorsExceed = true
-					cancel()
-					wg.Done()
-					return
-				}
-			default:
-			}
-
-			if step >= 0 {
-				taskChan <- tasks[step]
-				step--
-			}
-
-			if !(step >= 0) && len(taskChan) == 0 {
-				wg.Done()
-				cancel()
-				return
-			}
+	errCnt := 0
+	for _, t := range tasks {
+		select {
+		case <-errChan:
+			errCnt++
+		default:
 		}
-	}()
 
-	wg.Wait()
+		if !isIgnoreError && errCnt >= m {
+			isErrorsExceed = true
+			break
+		}
 
+		taskChan <- t
+	}
 	close(taskChan)
-	close(errChan)
+	wg.Wait()
 
 	if isErrorsExceed {
 		return ErrErrorsLimitExceeded
@@ -68,30 +47,13 @@ func Run(tasks []Task, n, m int) error {
 	return nil
 }
 
-func worker(
-	ctx context.Context,
-	isIgnoreErrors bool,
-	taskChan <-chan Task,
-	errChan chan<- struct{},
-	wg *sync.WaitGroup,
-) {
-	for {
-		select {
-		case <-ctx.Done():
-			wg.Done()
-			return
-		default:
-		}
-
-		select {
-		case <-ctx.Done():
-			wg.Done()
-			return
-		case task := <-taskChan:
-			err := task()
-			if !isIgnoreErrors && err != nil {
-				errChan <- struct{}{}
-			}
+func worker(isIgnoreErrors bool, taskChan <-chan Task, errChan chan<- struct{}, wg *sync.WaitGroup) {
+	for task := range taskChan {
+		err := task()
+		if !isIgnoreErrors && err != nil {
+			errChan <- struct{}{}
 		}
 	}
+
+	wg.Done()
 }
